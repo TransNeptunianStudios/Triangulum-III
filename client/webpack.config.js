85c7f199a027f25f0e03cae23dbfa85d0784c8b4
Robin Reicher <robin.o.reicher@gmail.com>
2017-01-07 02:01:35 +0100

    Revert "Added phaser-input through a script-loader and wired it up in Boot.js"

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -8,8 +8,6 @@ var phaser = path.join(phaserModule, 'build/custom/phaser-split.js')
 var pixi = path.join(phaserModule, 'build/custom/pixi.js')
 var p2 = path.join(phaserModule, 'build/custom/p2.js')
 
-var phaserInput = path.join(__dirname, '/node_modules/@orange-games/phaser-input');  
-
 var definePlugin = new webpack.DefinePlugin({
   __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || 'true'))
 })
@@ -38,21 +36,14 @@ module.exports = {
       server: {
         baseDir: ['./', './build']
       }
-    }),
+    })
   ],
   module: {
     loaders: [
       { test: /\.js$/, loader: 'babel', include: path.join(__dirname, 'src') },
       { test: /pixi\.js/, loader: 'expose?PIXI' },
       { test: /phaser-split\.js$/, loader: 'expose?Phaser' },
-      { test: /p2\.js/, loader: 'expose?p2' },
-      
-      // giving up here. fall back to script-loader for now.
-      {
-        test: /phaser-input/, loaders: [
-           'script-loader'
-        ]
-      }, 
+      { test: /p2\.js/, loader: 'expose?p2' }
     ]
   },
   node: {
@@ -62,8 +53,7 @@ module.exports = {
     alias: {
       'phaser': phaser,
       'pixi': pixi,
-      'p2': p2,
-      'phaser-input': phaserInput
+      'p2': p2
     }
   }
 }
