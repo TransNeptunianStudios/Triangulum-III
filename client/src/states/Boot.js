71ce9d7f17aa93603afc926109a6361377f83f42
Reicher <robin.o.reicher@gmail.com>
2017-01-07 17:55:43 +0100

    Added login to client

--- client/src/states/Boot.js
+++ client/src/states/Boot.js
@@ -1,17 +1,17 @@
 import Phaser from 'phaser'
-//@HACK: ensures Fabrique fron "phaser-input" gets exposed to window. 
+//@HACK: ensures Fabrique fron "phaser-input" gets exposed to window.
 require('phaser-input/build/phaser-input.js')
-
 
 export default class extends Phaser.State {
     init() {
       this.game.add.plugin(Fabrique.Plugins.InputField);
-      this.game.world.setBounds(0, 0, 2000, 2000)      
+      this.game.world.setBounds(0, 0, 2000, 2000)
     }
 
     preload() {
         this.load.image('TNSlogo', './assets/images/TNS_logo.png');
         this.load.image('starfield', './assets/images/tileable-classic-nebula-space-patterns-3.jpg');
+        this.load.image('enterButton', './assets/images/EnterButton.png');
 
         this.load.spritesheet('player', './assets/images/ship.png', 32, 32, 13);
     }
--- client/src/states/MainMenu.js
+++ client/src/states/MainMenu.js
@@ -15,9 +15,21 @@ export default class extends Phaser.State {
             fill: "#FFFFFF"
         }).anchor.set(0.5)
 
+        var input = this.game.add.inputField(0, 0);
+        input.x = this.game.width/2 - input.width/2
+        input.y = this.game.height/2- input.height/2 + 150
+
+        this.game.add.text(input.x, input.y-25, "Player name:", {
+            font: "20px Arial",
+            fill: "#FFFFFF"
+        })
+
+        var button = game.add.button(this.game.width/2, input.y +50, 'enterButton', this.startGame, this, 2, 1, 0);
+        button.anchor.set(0.5)
+
         this.player = new Player(this.game);
         this.player.login("Krabban");
-        this.game.input.onDown.addOnce(this.startGame, this);
+        //this.game.input.onDown.addOnce(this.startGame, this);
     }
 
     update() {}
