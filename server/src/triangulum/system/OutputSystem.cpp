7fab73da698bc616df406ec2c401abf90e0fb2b7
Mikael Larsson <c.mikael.larsson@gmail.com>
2017-01-27 23:32:25 +0100

    Removed debug print

--- server/src/triangulum/system/OutputSystem.cpp
+++ server/src/triangulum/system/OutputSystem.cpp
@@ -56,7 +56,7 @@ void OutputSystem::update(EntityManager& entities,
 
   resp_msg["objects"] = object_list;
 
-  std::cout << resp_msg.dump(2);
+  //std::cout << resp_msg.dump(2);
 
   entities.each<ClientInfo>([&resp_msg](Entity entity, ClientInfo& clientinfo) {
     // send to all clients
