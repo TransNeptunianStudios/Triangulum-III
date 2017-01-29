1d87cca091bce5051a7f08101bd73155336999ac
Anders Rydman <anders.rydman@gmail.com>
2017-01-29 13:53:06 +0100

    webpack production config had no script-loader set for phaser-input

--- client/webpack.production.config.js
+++ client/webpack.production.config.js
@@ -45,7 +45,12 @@ module.exports = {
       { test: /\.js$/, loader: 'babel', include: path.join(__dirname, 'src') },
       { test: /pixi\.js/, loader: 'expose?PIXI' },
       { test: /phaser-split\.js$/, loader: 'expose?Phaser' },
-      { test: /p2\.js/, loader: 'expose?p2' }
+      { test: /p2\.js/, loader: 'expose?p2' },
+      {
+        test: /phaser-input/, loaders: [
+          'script-loader'
+        ]
+      },
     ]
   },
   node: {
